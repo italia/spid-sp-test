819ecd2be12e89099c97776b684e9d12b2c5ceb9
peppelinux <giuseppe.demarco@unical.it>
2021-05-29 17:47:59 +0200

    feat: Test the compliance of ContactPerson Extensions element (Public)

--- src/spid_sp_test/metadata_public.py
+++ src/spid_sp_test/metadata_public.py
@@ -71,17 +71,55 @@ class SpidSpMetadataCheckPublic(object):
                 _msg = 'The TelephoneNumber element MUST start with “+39”'
                 self.handle_result('error', _msg, **error_kwargs)
 
-        ipacode = self.doc.xpath('//ContactPerson/Extensions/IPACode')
-        if not ipacode:
-            _msg = 'The IPACode element MUST be present'
+
+        if self.production:
+            ipacode = self.doc.xpath('//ContactPerson/Extensions/IPACode')
+            if not ipacode:
+                _msg = 'The IPACode element MUST be present'
+                self.handle_result('error', _msg, **error_kwargs)
+            elif ipacode:
+                ipacode = ipacode[0]
+                if not ipacode.text:
+                    _msg = 'The IPACode element MUST have a value'
+                    self.handle_result('error', _msg, **error_kwargs)
+                elif get_indicepa_by_ipacode(ipacode.text)[0] != 1:
+                    _msg = 'The IPACode element MUST have a valid value present on IPA '
+                    self.handle_result('error', _msg, **error_kwargs)
+
+        vat = self.doc.xpath('//ContactPerson/Extensions/VATNumber')
+        if vat:
+            if len(vat) != 1:
+                _msg = 'only one VATNumber element must be present'
+                self.handle_result('error', _msg, **error_kwargs)
+            vat = vat[0]
+            if not vat.text:
+                _msg = 'The VATNumber element MUST have a value'
+                self.handle_result('error', _msg, **error_kwargs)
+
+
+        fc = self.doc.xpath('//ContactPerson/Extensions/FiscalCode')
+        if fc:
+            if len(fc) != 1:
+                _msg = 'only one FiscalCode element must be present'
+                self.handle_result('error', _msg, **error_kwargs)
+            fc = fc[0]
+            if not fc.text:
+                _msg = 'The FiscalCode element MUST have a value'
+                self.handle_result('error', _msg, **error_kwargs)
+
+        ctype = self.doc.xpath('//ContactPerson/Extensions/Public')
+        if not ctype:
+            _msg = (f'Missing ContactPerson/Extensions/Public, '
+                    'this element MUST be present')
             self.handle_result('error', _msg, **error_kwargs)
-        elif ipacode:
-            ipacode = ipacode[0]
-            if not ipacode.text:
-                _msg = 'The IPACode element MUST have a value'
-                self.handle_result('error', _msg, **error_kwargs)
-            elif get_indicepa_by_ipacode(ipacode.text)[0] != 1:
-                _msg = 'The IPACode element MUST have a valid value present on IPA '
-                self.handle_result('error', _msg, **error_kwargs)
+        elif ctype[0].text:
+            _msg = (f'The Public element MUST be empty')
+            self.handle_result('error', _msg, **error_kwargs)
+
+        ctype = self.doc.xpath('//ContactPerson/Extensions/Private')
+        if ctype:
+            _msg = ('The Private element MUST not be present')
+            self.handle_result('error', _msg, **error_kwargs)
+
 
         return self.is_ok(f'{self.__class__.__name__}.test_Contacts_PubPriv')
