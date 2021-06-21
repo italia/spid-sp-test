cfd5d606d06aacaef52aefc826ba68a4c298e528
peppelinux <giuseppe.demarco@unical.it>
2021-06-21 18:24:39 +0200

    fix: ACR check won't fail if idp sends a level higher than the one requested by the sp

--- src/spid_sp_test/responses/response_mods.py
+++ src/spid_sp_test/responses/response_mods.py
@@ -5,3 +5,10 @@ from . settings import HTTP_STATUS_ERROR_CODES
 def dynamic_acr(check:SpidSpResponseCheck, response_obj:SpidSpResponse, **kwargs):
     if check.get_acr() != check.response_attrs['AuthnContextClassRef']:
         response_obj.conf['status_codes'] = HTTP_STATUS_ERROR_CODES
+        try:
+            level_sp = int(check.get_acr()[-1])
+            level_idp = int(check.response_attrs['AuthnContextClassRef'][-1])
+            if level_idp > level_sp:
+                response_obj.conf['status_codes'] = [200]
+        except Exception as e:
+            pass
