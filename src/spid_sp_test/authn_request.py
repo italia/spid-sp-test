f2aa880ccd4468ca6cfc1eca2573095ccdf0f1ef
Giuseppe <giuseppe.demarco@unical.it>
2021-07-28 18:19:06 +0200

    chore: authn request extra in a separate class

--- src/spid_sp_test/authn_request.py
+++ src/spid_sp_test/authn_request.py
@@ -616,47 +616,6 @@ class SpidSpAuthnReqCheck(AbstractSpidCheck):
             )
         return self.is_ok(f"{self.__class__.__name__}.test_AuthnRequest_SPID")
 
-    def test_AuthnRequest_SPID_extra(self):
-        """Test the compliance of AuthnRequest element"""
-
-        # ForceAuthn MUST be true if 'Comparison' is 'minimum' and
-        # SPID level is L1
-
-        req = self.doc.xpath("/AuthnRequest")
-        rac = None
-        acr = None
-        if req:
-            rac = req[0].xpath("./RequestedAuthnContext")
-        if rac:
-            acr = rac[0].xpath("./AuthnContextClassRef")
-
-        if req and rac and acr:
-            req = req[0]
-            rac = rac[0]
-            acr = acr[0]
-
-            if rac.get("Comparison") in ("minimum", "exact") and acr.text in (
-                "https://www.spid.gov.it/SpidL2",
-                "https://www.spid.gov.it/SpidL3",
-            ):
-                self._assertTrue(
-                    ("ForceAuthn" in req.attrib),
-                    "The ForceAuthn attribute MUST be present "
-                    "because of minimum/SpidL2",
-                    description=req.attrib,
-                )
-                self._assertTrue(
-                    req.get("ForceAuthn", "").lower() in ("true", 1, "1"),
-                    "The ForceAuthn attribute MUST be True "
-                    "because of minimum/SpidL2",
-                    description=req.attrib,
-                )
-        else:
-            self.handle_error(
-                "AuthnRequest or RequestAuthnContext or AuthnContextClassRef missing"
-            )
-
-        return self.is_ok(f"{self.__class__.__name__}.test_AuthnRequest_extra")
 
     def test_Subject(self):
         """Test the compliance of Subject element"""
@@ -1012,6 +971,5 @@ class SpidSpAuthnReqCheck(AbstractSpidCheck):
         self.test_Signature()
         self.test_xmldsig()
         self.test_AuthnRequest_SPID()
-        self.test_AuthnRequest_SPID_extra()
         self.test_NameIDPolicy()
         self.test_RequestedAuthnContext()
