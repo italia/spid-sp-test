93d64b208fdda0c944bcbc6ad27b2242e86a0d15
peppelinux <giuseppe.demarco@unical.it>
2021-10-05 22:25:37 +0200

    chore: authn request unit tests - faulty AssertionConsumerServiceURL

--- tests/test_06_authnrequest.py
+++ tests/test_06_authnrequest.py
@@ -41,3 +41,18 @@ def test_authnreq_spid_noacs_double_acs_url():
             metadata = UNSIGNED_METADATA,
             authn_request_url = _authn_req.encode()
         )
+
+def test_authnreq_spid_noacs_double_acs_faulty():
+    _authn_req = re.sub(
+        r'AssertionConsumerServiceURL=".*"',
+        'AttributeConsumingServiceIndex="0" AssertionConsumerServiceURL=""',
+        UNSIGNED_AUTHNREQ.decode()
+    )
+    md = load_authnreq(
+        metadata = UNSIGNED_METADATA,
+        authn_request_url = _authn_req.encode()
+    )
+    md.test_AuthnRequest_SPID()
+    _errors = [err['test'] for err in md.errors]
+    assert 'The AssertionConsumerServiceURL attribute MUST have a value' in _errors
+    assert 'The AssertionConsumerServiceURL attribute MUST be equal to an AssertionConsumerService Location' in _errors
